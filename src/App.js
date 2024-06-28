6cbf5e36652e7c229f8ff4f3a25451a397d67e9d
geno223 <eugenio.jimenez22397@gmail.com>
2024-06-28 18:15:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Pikachu</h1>
+     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"/>
+     <p>Pikachu is 1ft 4inches</p>
     </div>
   );
 }
