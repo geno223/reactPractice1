6ce39bcceca5e6a3269b45a5772e49c22d08281f
geno223 <eugenio.jimenez22397@gmail.com>
2024-07-06 13:12:13 -0400

    Exporting & Importing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Pikachu from './pikachu';
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Pikachu</h1>
-     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"/>
-     <p>Pikachu is 1ft 4inches</p>
-    </div>
+    <Pikachu/>
   );
 }
 
---
+++ src/pikachu.js
@@ -0,0 +1,13 @@
+
+
+function Pikachu(){
+    return(
+        <div className="App">
+     <h1>Pikachu</h1>
+     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"/>
+     <p>Pikachu is 1ft 4inches</p>
+    </div>
+    )
+}
+
+export default Pikachu;
